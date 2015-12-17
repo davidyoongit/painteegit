c7ecb7fa8b688c53671ffda4247d1eadddc48587
davidyoongit <g.dream.yoon@gmail.com>
2015-12-17 17:22:31 +0900

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-var express = require('express')
+var express = require('express');
 var path = require('path');
 var app= express();
+// var mongoose = require('mongoose');
+// var mongodb = require('mongodb');
+// var uriUtil = require('mongodb-uri');
 
 app.use(express.static(path.join(__dirname + '/public')));
 
-// app.get('/', function (req,res) {
-//   res.send('Hello World!');
+// var mongoUri = "mongodb://david:davidyoon@ds043714.mongolab.com:43714/paintee";
+// var mongooseUri = uriUtil.formatMongoose(mongoUri);
+// var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000, auto_reconnect: true } },
+//                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+// mongoose.connect(mongooseUri,options);
+//
+// var conn = mongoose.connection;
+// conn.once("open", function () {
+//   console.log("DB connected!");
 // });
+// conn.once("error", function(err){
+//   console.log("online DB error:",err);
+// });
+
+ app.get('/', function (req,res) {
+//   res.send('Hello World!');
+ });
 
 app.listen(3000, function () {
   console.log('Server On!');
